7ab1e414dc28e02ae0776bad206bfb5ebde98f96
Boris Lysikov <blysikov@vivid.money>
2022-04-09 15:09:40 +0700

    add runing smoke and perfomance test in fastfile

---
+++ Tests/HackersUITests/Tests/PerfomanceTests.swift
@@ -0,0 +1,13 @@
+//
+//  PerfomanceTests.swift
+//  HackersUITests
+//
+//  Created by Boris Lysikov on 09.04.2022.
+//  Copyright © 2022 Glass Umbrella. All rights reserved.
+//
+
+import XCTest
+
+final class SmokeTests: CommonTest {
+    
+}
---
+++ Tests/HackersUITests/Tests/SmokeTests.swift
@@ -0,0 +1,21 @@
+//
+//  SmokeTests.swift
+//  HackersUITests
+//
+//  Created by Boris Lysikov on 09.04.2022.
+//  Copyright © 2022 Glass Umbrella. All rights reserved.
+//
+
+import XCTest
+
+final class SmokeTests: CommonTest {
+    
+    func testLogIn() {
+        
+    }
+    
+    func testOpenPostDetails() {
+        
+    }
+    
+}
