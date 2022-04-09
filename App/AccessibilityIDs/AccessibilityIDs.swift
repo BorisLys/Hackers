bba186cee16c01a8f3539468b0d4d8eaa81d26dc
Boris Lysikov <blysikov@vivid.money>
2022-04-09 11:23:59 +0700

    Add IDs for settings screen

---
+++ App/AccessibilityIDs/AccessibilityIDs.swift
@@ -0,0 +1,51 @@
+//
+//  AccessibilityIDs.swift
+//  Hackers
+//
+//  Created by Boris Lysikov on 09.04.2022.
+//  Copyright © 2022 Glass Umbrella. All rights reserved.
+//
+
+import Foundation
+
+public struct AccessibilityIDs {
+
+    public enum SettingsScreen {
+        
+        enum StaticText: AccessibilityIDProviderProtocol {
+            case accountLabel
+            case usernameLabel
+            case versionLabel
+            case gitHubLabel
+            case feedbackLabel
+            case whatsNewLabel
+            case appNameLabel
+            case authorNameLabel
+            case openInDefaultBrowserLabel
+        }
+        
+        enum Button: AccessibilityIDProviderProtocol {
+            case showThumbnailsSwitch
+            case swipeActionsSwitch
+            case safariReaderModeSwitch
+            case openInDefaultBrowserSwitch
+        }
+        
+        enum Image: AccessibilityIDProviderProtocol {
+            case appImage
+        }
+    }
+    
+    public enum FeedScreen {
+        
+    }
+    
+    public enum FeedDetailsScreen {
+        enum StaticText: AccessibilityIDProviderProtocol {
+            case authorLabel
+            case datePostedLabel
+        }
+        
+    }
+}
+
